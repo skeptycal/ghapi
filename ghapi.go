59296a9881aeecadfe96be9c587930f0babd09b8
Michael Treanor <26148512+skeptycal@users.noreply.github.com>
2021-05-01 12:53:58 -0500

    GoBot: setup repo files from template

---
+++ ghapi.go
@@ -0,0 +1,6 @@
+// Copyright (c) 2021 Michael Treanor
+// https://github.com/skeptycal
+// MIT License
+
+// Package ghapi contains utilities for macOS.
+package ghapi
--- idea.md
+++ idea.md
@@ -1,4 +1,4 @@
-# gorepotemplate
+# ghapi
 
 > Tricky and fun utilities for Go programs on macOS.
 
